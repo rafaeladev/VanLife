7183c75e91c9c0ce105b231a98d951550d9bd91a
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-03-16 12:50:31 +0100

    first Vans page

--- src/Vans.js
+++ src/Vans.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Vans = () => {
-    return <div></div>;
-};
+export default function Vans() {
+    const [vans, setVans] = React.useState([]);
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then((res) => res.json())
+            .then((data) => setVans(data.vans));
+    }, []);
 
-export default Vans;
+    const vanElements = vans.map((van) => (
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} alt="vans" />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>
+                    ${van.price}
+                    <span>/day</span>
+                </p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ));
+
+    return (
+        <div className="van-list-container">
+            <div className="van-list">{vanElements}</div>
+        </div>
+    );
+}
